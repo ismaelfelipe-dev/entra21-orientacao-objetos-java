2ff5af13473556b203f876ad428867924d5561bd
Guest Blumenau <guest.bu@alunos.sc.senac.br>
2026-05-20 22:03:24 -0300

    feat: adiciona o projeto / exercício

--- src/main/java/org/example/classes/Beneficiario.java
+++ src/main/java/org/example/classes/Beneficiario.java
@@ -1,4 +1,34 @@
 package org.example.classes;
 
-public class Beneficiario {
+import java.time.LocalDate;
+import java.time.Period;
+
+public abstract class Beneficiario  {
+    private String nome;
+    private String cpf;
+    private LocalDate dataDeNascimento;
+
+    public Beneficiario(String nome, String cpf, LocalDate dataDeNascimento) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public LocalDate getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public int idade(){
+        return Period.between(dataDeNascimento, LocalDate.now()).getYears();
+    }
+
+    public abstract double calcularMensalidade();
 }
--- src/main/java/org/example/classes/Titular.java
+++ src/main/java/org/example/classes/Titular.java
@@ -1,4 +1,19 @@
 package org.example.classes;
 
-public class Titular {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Titular extends Beneficiario {
+
+    private List<Dependente> dependentes = new ArrayList<>();
+
+    public Titular(String nome, String cpf, LocalDate dataDeNascimento) {
+        super(nome, cpf, dataDeNascimento);
+    }
+
+    @Override
+    public double calcularMensalidade() {
+        return 0;
+    }
 }
